4c0e6b07b47d17c92b6f298f086168e466d6129f
sub6550 <sub6550@gmail.com>
2025-11-17 14:05:30 +0900

    25.11.17 오전 배포

---
+++ src/main/java/kr/co/busanbank/service/CsService.java
@@ -0,0 +1,34 @@
+package kr.co.busanbank.service;
+
+import kr.co.busanbank.dto.CodeDetailDTO;
+import kr.co.busanbank.dto.FaqDTO;
+import kr.co.busanbank.dto.PageRequestDTO;
+import kr.co.busanbank.dto.PageResponseDTO;
+import kr.co.busanbank.mapper.CsMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CsService {
+
+    private final CsMapper csMapper;
+
+    public PageResponseDTO<FaqDTO> getFaqList(PageRequestDTO pageRequestDTO) {
+
+        List<FaqDTO> dtoList = csMapper.selectFaqList(pageRequestDTO);
+        int total = csMapper.selectFaqTotal(pageRequestDTO);
+
+        return PageResponseDTO.<FaqDTO>builder()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total(total)
+                .build();
+    }
+    
+    public List<CodeDetailDTO> getFaqCategories() {
+        return csMapper.selectFaqCategories();
+    }
+}
